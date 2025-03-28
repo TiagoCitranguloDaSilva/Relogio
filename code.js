29e40c67d7f3a6ce647dff4881ae0b0bfece6497
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2025-03-28 13:29:32 -0300

    Resolvendo problema do horario

--- code.js
+++ code.js
@@ -16,15 +16,28 @@ var segundos = data.getSeconds()
 
 
 var rotacaoSegundo = segundos
-var rotacaoMinuto = minutos * 6 + segundos * 0.5
-var rotacaoHora = hora * 30 + minutos * 6 + segundos * (1 / 120)
+var rotacaoMinuto = minutos * 6
+var rotacaoHora = hora * 30
+ponteiroSegundo.style.rotate = `${rotacaoSegundo}deg`
+ponteiroMinuto.style.rotate = `${rotacaoMinuto}deg`
+ponteiroHora.style.rotate = `${rotacaoHora}deg`
+
 setInterval(() => {
   rotacaoSegundo+= 6
   ponteiroSegundo.style.rotate = `${rotacaoSegundo}deg`
-  rotacaoMinuto += 0.1
-  ponteiroMinuto.style.rotate = `${rotacaoMinuto}deg`
-  rotacaoHora += 0.00833
-  ponteiroHora.style.rotate = `${rotacaoHora}deg`
+  
 }, 1000)
+
+setInterval(() => {
+  rotacaoMinuto += 6
+  ponteiroMinuto.style.rotate = `${rotacaoMinuto}deg`
+  
+}, 60000);
+
+setInterval(() => {
+  rotacaoHora += 30
+  ponteiroHora.style.rotate = `${rotacaoHora}deg`
+}, 3600000)
+
 
 
